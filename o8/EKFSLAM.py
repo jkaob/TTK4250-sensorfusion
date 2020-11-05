b60b6f1ddf07368eddba4fb8a52a6b44e1517cd4
jakob G <j.grepperud@gmail.com>
2020-11-05 15:15:08 +0100

    1h

--- o8/EKFSLAM.py
+++ o8/EKFSLAM.py
@@ -21,6 +21,7 @@ class EKFSLAM:
         do_asso=False,
         alphas=np.array([0.001, 0.0001]),
         sensor_offset=np.zeros(2),
+        Q_process = np.empty_like(Q)
     ):
 
         self.Q = Q
@@ -29,14 +30,16 @@ class EKFSLAM:
         self.alphas = alphas
         self.sensor_offset = sensor_offset
 
+        self.Q_process = Q_process
+
+
     def f(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
         """Add the odometry u to the robot state x.
         Parameters
-        ----------
-        x : np.ndarray, shape=(3,) the robot state
-        u : np.ndarray, shape=(3,) the odometry
+         x : np.ndarray, shape=(3,) the robot state
+         u : np.ndarray, shape=(3,) the odometry
         Returns:
-        np.ndarray, shape = (3,) the predicted state
+         np.ndarray, shape = (3,) the predicted state
         """
         # eq (11.7). Should wrap heading angle between (-pi, pi), see utils.wrapToPi
         #psi   = wrapToPi(x[2])
@@ -50,11 +53,10 @@ class EKFSLAM:
     def Fx(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
         """Calculate the Jacobian of f with respect to x.
         Parameters
-        ----------
-        x : np.ndarray, shape=(3,) the robot state
-        u : np.ndarray, shape=(3,) the odometry
+         x : np.ndarray, shape=(3,) the robot state
+         u : np.ndarray, shape=(3,) the odometry
         Returns:
-        np.ndarray. The Jacobian of f wrt. x.
+         np.ndarray. The Jacobian of f wrt. x.
         """
         Fx = np.array([ [1, 0, -u[0]*np.sin(x[2]) - u[1]*np.cos(x[2]) ] ,
                         [0, 1,  u[0]*np.cos(x[2]) - u[1]*np.sin(x[2]) ] ,
@@ -65,14 +67,11 @@ class EKFSLAM:
 
     def Fu(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
         """Calculate the Jacobian of f with respect to u.
-
         Parameters
-        ----------
-        x : np.ndarray, shape=(3,)the robot state
-        u : np.ndarray, shape=(3,)the odometry
+         x : np.ndarray, shape=(3,)the robot state
+         u : np.ndarray, shape=(3,)the odometry
         Returns
-        -------
-        np.ndarray The Jacobian of f wrt. u.
+         np.ndarray The Jacobian of f wrt. u.
         """
         Fu = np.array([ [np.cos(x[2]), -np.sin(x[2]), 0] ,
                         [np.sin(x[2]),  np.cos(x[2]), 0] ,
@@ -87,13 +86,12 @@ class EKFSLAM:
         """Predict the robot state using the zOdo as odometry the corresponding state&map covariance.
 
         Parameters
-        ----------
-        eta : np.ndarray, shape=(3 + 2*#landmarks,) :the robot state and map concatenated
-        P : np.ndarray, shape=(3 + 2*#landmarks,)*2 :the covariance of eta
-        z_odo : np.ndarray, shape=(3,) :the measured odometry
+         eta   : np.ndarray, shape=(3 + 2*#landmarks,) :the robot state and map concatenated
+         P     : np.ndarray, shape=(3 + 2*#landmarks,)*2 :the covariance of eta
+         z_odo : np.ndarray, shape=(3,) :the measured odometry
 
         Returns:
-        Tuple[np.ndarray, np.ndarray], shapes= (3 + 2*#landmarks,), (3 + 2*#landmarks,)*2
+         Tuple[np.ndarray, np.ndarray], shapes= (3 + 2*#landmarks,), (3 + 2*#landmarks,)*2
             predicted mean and covariance of eta.
         """
         # check inout matrix
@@ -115,13 +113,7 @@ class EKFSLAM:
         Fu = self.Fu(etapred[:3], z_odo)
 
         # evaluate covariance prediction in place to save computation
-        # only robot state changes, so only rows and colums of robot state needs changing
-        # cov matrix layout:
-        # [[P_xx, P_xm],
-        # [P_mx, P_mm]]
-
-        #P[:3, :3] = Fx @ P[:3,:3] @ Fx.T + self.Q # robot cov prediction
-        P[:3, :3] = Fx @ P[:3,:3] @ Fx.T + Fu @ self.Q @ Fu.T # G = Fu ?
+        P[:3, :3] = Fx @ P[:3,:3] @ Fx.T + Fu @ self.Q @ Fu.T + self.Q_process
         P[:3, 3:] = Fx @ P[:3,3:]   # robot-map covariance prediction
         P[3:, :3] = P[:3, 3:].T #P[3:,:3] @ Fx.T # map-robot covariance: transpose of the above
 
@@ -137,32 +129,29 @@ class EKFSLAM:
     def h(self, eta: np.ndarray) -> np.ndarray:
         """Predict all the landmark positions in sensor frame.
         Parameters
-        ----------
-        eta : (3 + 2 * #landmarks,) The robot state and landmarks stacked.
-
+         eta : (3 + 2 * #landmarks,) The robot state and landmarks stacked.
         Returns
-        np.ndarray, shape=(2 * #landmarks,) The landmarks in the sensor frame.
+         np.ndarray, shape=(2 * #landmarks,) The landmarks in the sensor frame.
         """
         x = eta[:3]
         ## reshape map (2, #landmarks), m[:, j] is the jth landmark
         m = eta[3:].reshape((-1, 2)).T
-        R = rotmat2d(-x[2])
+        R = rotmat2d(-x[2])  #  world->body
+                             # R.T: body->world
 
-        # None as index ads an axis with size 1 at that position.
-        # Numpy broadcasts size 1 dimensions to any size when needed (???)
+        # relative position of landmark to sensor on robot (world)
+        delta_m     = m - x[:2]  # No sensor offset here
 
-        # relative position of landmark to sensor on robot in world frame
-        delta_m     = m - x[:2]
+        # predicted measurements in cartesian coordinates
+        zpredcart   = R @ delta_m - self.sensor_offset #body
 
-        # predicted measurements in cartesian coordinates, beware sensor offset for VP
-        zpredcart   = delta_m - R @ self.sensor_offset
-
-        zpred_r     = la.norm(zpredcart) # ranges
-        zpred_theta = np.arctan(zpredcard[1], zpredcart[0]) # bearings
+        # Converting cartesian body -> polar body
+        zpred_r     = la.norm(zpredcart)
+        zpred_theta = np.arctan(zpredcard[1], zpredcart[0])
         zpred       = np.vstack(zpred_r, zpred_theta)
-        # [ranges; bearings] shape (2, #lmrk)
 
-        zpred = zpred.T.ravel() # stack measurements along one dimension, [range1 bearing1 range2 bearing2 ...]
+        # stack measurements along one dimension, [range1 bearing1 range2 bearing2 ...]
+        zpred = zpred.T.ravel()
 
         assert (
             zpred.ndim == 1 and zpred.shape[0] == eta.shape[0] - 3
@@ -180,21 +169,20 @@ class EKFSLAM:
         x       = eta[:3]
         m       = eta[3:].reshape((-1, 2)).T ## reshape map (2, #landmarks), m[j] is the jth landmark
         numM    = m.shape[1]
-        R       = rotmat2d(x[2])
-        R_neg   = rotmat2d(-x[2])
+        R       = rotmat2d(x[2])  # body->world
 
-        delta_m = m - x[:2] # relative position of landmark to robot in world frame. m - rho that appears in (11.15) and (11.16)
-        zc      = delta_m - R @ self.sensor_offset
+        # relative position of landmark to robot in world frame. m - rho that appears in (11.15) and (11.16)
+        delta_m = m - x[:2]  # (world)
+        zc      = delta_m - R @ self.sensor_offset # body cartesian
         #, (2, #measurements), each measured position in cartesian coordinates like
         # [x coordinates; y coordinates]
 
         zpred   = self.h(eta)
         #(2, #measurements), predicted measurements, like
-        # [ranges; bearings]    # dette skjer jo ikke
-
+        # [ranges; bearings]   # dette skjer jo ikke
         print("zpred shape: ", zpred.shape)
 
-        # every other element is range?
+        # every other element is range
         zr      = np.array([zpred[i] for i in range(0, numM, 2)]) # ranges
 
         Rpihalf = rotmat2d(np.pi / 2)
@@ -220,101 +208,97 @@ class EKFSLAM:
             Hx[inds]        = np.vstack((zc.T / la.norm(zc)) @ temp,
                                         (zc.T @ Rpihalf.T / la.norm(zc)**2) @ temp)
 
+            #TODO : avoid making this for all the landmarks you _know_
+            # you will not detect (the maximum range should be available from the data).
             Hm[inds, inds]  = np.vstack(delta_m[i].T * 1 / la.norm(delta_m[i]),
                                         delta_m[i].T @ Rpihalf * 1 / (la.norm(delta_m[i])**2))
 
-        # TODO: You can set some assertions here to make sure that some of the structure in H is correct
+        #set some assertions here to make sure that some of the structure in H is correct
         assert (H.shape[0] == 2*numM and H.shape[1] ==  3+2*numM), "SLAM.H: Wrong shape of H"
+        assert (Hx.shape[0] == 2*numM and Hx.shape[1] == 3), "SLAM.H: Wrong shape of Hx"
         return H
 
     def add_landmarks(
         self, eta: np.ndarray, P: np.ndarray, z: np.ndarray
     ) -> Tuple[np.ndarray, np.ndarray]:
         """Calculate new landmarks, their covariances and add them to the state.
-
         Parameters
-        ----------
-        eta : np.ndarray, shape=(3 + 2*#landmarks,)  the robot state and map concatenated
-        P : np.ndarray, shape=(3 + 2*#landmarks,)*2  the covariance of eta
-        z : np.ndarray, shape(2 * #newlandmarks,)   A set of measurements to create landmarks for
-
+         eta : np.ndarray, shape=(3 + 2*#landmarks,)  the robot state and map concatenated
+         P : np.ndarray, shape=(3 + 2*#landmarks,)*2  the covariance of eta
+         z : np.ndarray, shape(2 * #newlandmarks,)   A set of measurements to create landmarks for
         Returns
-        -------
-        Tuple[np.ndarray, np.ndarray], shapes=(3 + 2*(#landmarks + #newlandmarks,), (3 + 2*(#landmarks + #newlandmarks,)*2
+         Tuple[np.ndarray, np.ndarray], shapes=(3 + 2*(#landmarks + #newlandmarks,), (3 + 2*(#landmarks + #newlandmarks,)*2
             eta with new landmarks appended, and its covariance
         """
         n = P.shape[0]
         assert z.ndim == 1, "SLAM.add_landmarks: z must be a 1d array"
 
-        numLmk = z.shape[0] // 2
+        numLmk  = z.shape[0] // 2
+        lmnew   = np.empty_like(z)
 
-        lmnew = np.empty_like(z)
-
-        Gx = np.empty((numLmk * 2, 3))
-        Rall = np.zeros((numLmk * 2, numLmk * 2))
+        Gx      = np.empty((numLmk * 2, 3))
+        Rall    = np.zeros((numLmk * 2, numLmk * 2))
 
         I2 = np.eye(2) # Preallocate, used for Gx
         sensor_offset_world = rotmat2d(eta[2]) @ self.sensor_offset # For transforming landmark position into world frame
         sensor_offset_world_der = rotmat2d(eta[2] + np.pi / 2) @ self.sensor_offset # Used in Gx
 
         for j in range(numLmk):
-            ind = 2 * j
-            inds = slice(ind, ind + 2)
-            zj = z[inds]
+            ind     = 2 * j
+            inds    = slice(ind, ind + 2)
+            zj      = z[inds]
 
-            rot = # TODO, rotmat in Gz
-            lmnew[inds] = # TODO, calculate position of new landmark in world frame
+            rot          = rotmat2d(zj[1] + eta[2])  # R(zj_angle + psi) #rotmat in Gz
+            lmnew[inds]  = rotmat2d(eta[2]) @ lmnew[inds] # calculate position of new landmark in world frame
 
-            Gx[inds, :2] = # TODO
-            Gx[inds, 2] = # TODO
+            Gx[inds, :2] = I2
+            Gx[inds, 2]  = zj[0] @ np.vstack(-np.sin(zj[1]+eta[2]), np.cos(zj[1]+eta[2])) + sensor_offset_world_der
+            Gz           = rot @ np.diag((1, zj[0]))
 
-            Gz = # TODO
-
-            Rall[inds, inds] = # TODO, Gz * R * Gz^T, transform measurement covariance from polar to cartesian coordinates
+            # Gz * R * Gz^T, transform measurement covariance from polar to cartesian coordinates
+            Rall[inds, inds] = Gz @ self.R @ Gz.T
 
         assert len(lmnew) % 2 == 0, "SLAM.add_landmark: lmnew not even length"
-        etaadded = # TODO, append new landmarks to state vector
-        Padded = # TODO, block diagonal of P_new, see problem text in 1g) in graded assignment 3
-        Padded[n:, :n] = # TODO, top right corner of P_new
-        Padded[:n, n:] = # TODO, transpose of above. Should yield the same as calcualion, but this enforces symmetry and should be cheaper
+
+        etaadded        = eta # [eta; G() ] : append new landmarks to state vector
+        etaadded[3:]    = lmnew # ??
+
+        low_r           = Gx @ P[:3,:3] @ Gx.T + Rall # + Gz @ self.R @ Gz.T
+        P_added         = la.block_diag(P, low_r)     # block diagonal of P_new
+        P_added[n:, :n] = P[:,:3] @ Gx.T      # top right corner of P_new
+        P_added[:n, n:] = P_added[n:, :n].T   # transpose of above
 
         assert (
-            etaadded.shape * 2 == Padded.shape
+            etaadded.shape * 2 == P_added.shape
         ), "EKFSLAM.add_landmarks: calculated eta and P has wrong shape"
         assert np.allclose(
-            Padded, Padded.T
-        ), "EKFSLAM.add_landmarks: Padded not symmetric"
+            P_added, P_added.T
+        ), "EKFSLAM.add_landmarks: P_added not symmetric"
         assert np.all(
-            np.linalg.eigvals(Padded) >= 0
-        ), "EKFSLAM.add_landmarks: Padded not PSD"
+            np.linalg.eigvals(P_added) >= 0
+        ), "EKFSLAM.add_landmarks: P_added not PSD"
 
-        return etaadded, Padded
+        return etaadded, P_added
 
     def associate(
         self, z: np.ndarray, zpred: np.ndarray, H: np.ndarray, S: np.ndarray,
     ):  # -> Tuple[*((np.ndarray,) * 5)]:
         """Associate landmarks and measurements, and extract correct matrices for these.
-
         Parameters
-        ----------
-        z : np.ndarray,
+         z : np.ndarray,
             The measurements all in one vector
-        zpred : np.ndarray
+         zpred : np.ndarray
             Predicted measurements in one vector
-        H : np.ndarray
+         H : np.ndarray
             The measurement Jacobian matrix related to zpred
-        S : np.ndarray
+         S : np.ndarray
             The innovation covariance related to zpred
-
         Returns
-        -------
-        Tuple[*((np.ndarray,) * 5)]
+         Tuple[*((np.ndarray,) * 5)]
             The extracted measurements, the corresponding zpred, H, S and the associations.
-
         Note
-        ----
-        See the associations are calculated  using JCBB. See this function for documentation
-        of the returned association and the association procedure.
+         See the associations are calculated  using JCBB. See this function for documentation
+         of the returned association and the association procedure.
         """
         if self.do_asso:
             # Associate
